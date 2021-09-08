ceb32b638dcffc1c6d63acfcc3760efcbc987a68
Julia Voronetskaja <julia.voronetskaja@tthk.ee>
2021-09-08 09:42:50 +0300

    Check Year Of Birth

---
+++ HowOldAreYou/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HowOldAreYou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
